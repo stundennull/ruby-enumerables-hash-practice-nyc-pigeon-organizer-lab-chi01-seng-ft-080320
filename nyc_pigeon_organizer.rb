e806940f959839e6c23ecee4dad612d509bcbfe1
Kyle Walters <kyle.p.walters@gmail.com>
2020-07-31 20:48:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = data.each_with_object ({}) do |(key, value), new_array| 
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !new_array[name]
+          new_array[name] = {}
+        end
+        if !new_array[name][key]
+          !new_array[name][key] = []
+        end
+  new_array[name][key].push(inner_key)
+   end
+  end
+ end
 end
